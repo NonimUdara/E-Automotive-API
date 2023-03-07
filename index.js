e902aa71b04c8db458d148f592eaa22af7dffec5
Nonim Udara <nonimudara123@gmail.com>
2023-03-07 16:11:58 +0530

    code changes of api

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const userRoutes = require("./controllers/admincontroller");
 
+const authRoutes = require("./controllers/authcontroller");
+const userRoutes = require("./controllers/users");
+
+const postRoutes = require('./routes/posts');
 
 const app = express();
 
@@ -14,7 +17,10 @@ app.use(cors());
 app.use(express.json());
 
 //route middleware
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(postRoutes);
 
 const PORT = 8000;
 const DB_URL = 'mongodb+srv://Udara:udara123@mernapp.ypcrk.mongodb.net/e-automotive?retryWrites=true&w=majority'
