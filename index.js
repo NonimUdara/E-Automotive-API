58ccb58c73452d9b3be5434ac819d8ae5cff540d
Nonim Udara <nonimudara123@gmail.com>
2023-03-29 11:27:35 +0530

    update index

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-require("dotenv").config();
+const dotenv = require('dotenv');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+dotenv.config();
+
+mongoose.set('strictQuery', false);
 
 const authRoutes = require("./controllers/adminLoginController");
 const userRoutes = require("./controllers/userRegisterController");
@@ -28,8 +32,8 @@ const PORT = 8000;
 const DB_URL = 'mongodb+srv://Udara:udara123@mernapp.ypcrk.mongodb.net/e-automotive?retryWrites=true&w=majority'
 
 mongoose.connect(DB_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
 })
 .then(() =>{
     console.log('DB Connected');
