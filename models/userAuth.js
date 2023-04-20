5aea7f8c4b48eb851a3709b08cf04186096e73b9
Nonim Udara <nonimudara123@gmail.com>
2023-04-20 12:31:01 +0530

    add comment

--- models/userAuth.js
+++ models/userAuth.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
 	password: { type: String, required: true },
 	image: { type: imageSchema }
 });
-
+//kz
 userSchema.methods.generateAuthToken = function () {
 	const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
 		expiresIn: "7d",
