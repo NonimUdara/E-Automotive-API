839418f75cd6353f7fa04c894b453c83a5b52fff
Nonim Udara <87659260+NonimUdara@users.noreply.github.com>
2025-01-17 22:27:24 +0530

    update stripe publishable key and secret key

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -2,8 +2,14 @@
 const Stripe = require('stripe');
 const express = require('express');
 const app = express.Router();
-const PUBLISHABLE_KEY = "pk_test_51N1DBBIcQPaMmd0Kitgwpu1HRAZxuFKrdac53qE4alOJZ5GURZkFuDfcPmCqFVFdUHXjTyOuX5Um31ffNeBavZyz00pIOY6omo";
-const SECRET_KEY = "sk_test_51N1DBBIcQPaMmd0K4rV3tebawAORe1YPUJZYpF2myAdLuKECLSGH74wry09Me4yG1z6rPkS382LOcWumCqnGjEf1005mL0cbVD";
+
+// const PUBLISHABLE_KEY = "pk_test_51N1DBBIcQPaMmd0Kitgwpu1HRAZxuFKrdac53qE4alOJZ5GURZkFuDfcPmCqFVFdUHXjTyOuX5Um31ffNeBavZyz00pIOY6omo";
+// const SECRET_KEY = "sk_test_51N1DBBIcQPaMmd0K4rV3tebawAORe1YPUJZYpF2myAdLuKECLSGH74wry09Me4yG1z6rPkS382LOcWumCqnGjEf1005mL0cbVD";
+
+//update stripe publishable key and secret key
+
+const PUBLISHABLE_KEY = "pk_test_51QiFa22NxTyhzQ4xy1JRcdt8dGHYCPQmb4IMvcUlX2HyZpJDFl4HnPektBP89FmPsc0ge4ONiTCWvMeabvAqHUDz00Mh9LV9qW";
+const SECRET_KEY = "sk_test_51QiFa22NxTyhzQ4x6midlw9kKQpk9F4aRa3T9B1ERaHAf8TfV3mRH7BW6Y3rpcInILMyrmK29qm3QGvQtWG1NSGh00TZ3rrOFb";
 
 //Confirm the API version from your stripe dashboard
 const stripe = Stripe(SECRET_KEY, { apiVersion: "2020-08-27" });
